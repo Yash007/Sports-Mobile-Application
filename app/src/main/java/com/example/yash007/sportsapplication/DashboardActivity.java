b63ee790e4ec278f8eed9eaaa566fa2aa03aa1d3
Yash007 <sompurayash@live.com>
2018-03-07 16:49:41 -0500

    Create Team activity added

--- app/src/main/java/com/example/yash007/sportsapplication/DashboardActivity.java
+++ app/src/main/java/com/example/yash007/sportsapplication/DashboardActivity.java
@@ -2,17 +2,31 @@ package com.example.yash007.sportsapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class DashboardActivity extends AppCompatActivity {
 
+    public FloatingActionButton createTeam;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        createTeam = (FloatingActionButton) findViewById(R.id.createTeamButton);
+
+
+        createTeam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),CreateTeamActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -42,6 +56,8 @@ public class DashboardActivity extends AppCompatActivity {
         }
         return true;
     }
+
+
 
 
 }
