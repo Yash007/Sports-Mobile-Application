7a35fb94a1038dbc9048deb7b1699331d2e6569e
Yash007 <sompurayash@live.com>
2018-03-13 15:56:32 -0400

    Sign up Function has been integrated with REST Webservice now.

---
+++ app/src/main/java/com/example/yash007/sportsapplication/Config.java
@@ -0,0 +1,9 @@
+package com.example.yash007.sportsapplication;
+
+/**
+ * Created by yash007 on 2018-03-13.
+ */
+
+public class Config {
+    public static final String webUrl = "https://sporties.herokuapp.com/api/";
+}
--- app/src/main/java/com/example/yash007/sportsapplication/SignupActivity.java
+++ app/src/main/java/com/example/yash007/sportsapplication/SignupActivity.java
@@ -1,13 +1,194 @@
 package com.example.yash007.sportsapplication;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Iterator;
 
 public class SignupActivity extends AppCompatActivity {
+
+    private EditText firstName, lastName, email, password, confirmPassword, phone;
+    private Spinner gender;
+    private ProgressDialog pDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        firstName = (EditText) findViewById(R.id.firstName);
+        lastName = (EditText) findViewById(R.id.lastName);
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        confirmPassword = (EditText) findViewById(R.id.confirmPassword);
+        phone = (EditText) findViewById(R.id.phoneNumber);
+        gender = (Spinner) findViewById(R.id.gender);
+
+    }
+
+    public void doSignUp(View v)  {
+        if(password.getText().toString().equals(confirmPassword.getText().toString()) == true)  {
+            //code for sign up
+            new SignUpPost().execute();
+        }
+        else    {
+            Toast.makeText(SignupActivity.this, "Password must be same", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public class SignUpPost extends AsyncTask<String, Void, String> {
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(SignupActivity.this);
+            pDialog.setMessage("Please wait...");
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        protected String doInBackground(String... arg0) {
+
+            try {
+
+                //URL url = new URL("https://studytutorial.in/post.php");
+                URL url = new URL(Config.webUrl+"createUser");
+
+                JSONObject postDataParams = new JSONObject();
+
+                postDataParams.put("pFirstName", firstName.getText().toString());
+                postDataParams.put("pLastName", lastName.getText().toString());
+                postDataParams.put("pEmail",email.getText().toString());
+                postDataParams.put("pBirthday","");
+                postDataParams.put("pPassword",password.getText().toString());
+                postDataParams.put("pLoginType","Form");
+                postDataParams.put("pAuthenticated","fale");
+                postDataParams.put("pAccountStatus","true");
+                postDataParams.put("pAddress","");
+                postDataParams.put("pBio","");
+                postDataParams.put("pHeight","");
+                postDataParams.put("pWeight","");
+
+
+                Log.e("params",postDataParams.toString());
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(15000 /* milliseconds */);
+                conn.setConnectTimeout(15000 /* milliseconds */);
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(getPostDataString(postDataParams));
+
+                writer.flush();
+                writer.close();
+                os.close();
+
+                int responseCode=conn.getResponseCode();
+
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+
+                    BufferedReader in=new BufferedReader(new
+                            InputStreamReader(
+                            conn.getInputStream()));
+
+                    StringBuffer sb = new StringBuffer("");
+                    String line="";
+
+                    while((line = in.readLine()) != null) {
+
+                        Log.e("+++++", "line: "+line);
+                        sb.append(line);
+                        //break;
+                    }
+
+                    in.close();
+                    return sb.toString();
+
+                }
+                else {
+                    return new String("false : "+responseCode);
+                }
+            }
+            catch(Exception e) {
+                Log.e("~~~", e.toString());
+                return new String("Exception: " + e.getMessage());
+            }
+
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.d("TAG",result);
+            String status = null;
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                status = jsonObject.getString("Status");
+                //JSONObject profile = jsonObject.
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            pDialog.dismiss();
+
+            if(status.equals("Success") == true) {
+                Toast.makeText(getApplicationContext(),"Signup successfully. Please login now",Toast.LENGTH_LONG).show();
+
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+            else    {
+                Toast.makeText(getApplicationContext(),"Incorrect username or password",Toast.LENGTH_LONG).show();
+            }
+
+
+        }
+    }
+
+    public String getPostDataString(JSONObject params) throws Exception {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        Iterator<String> itr = params.keys();
+        while(itr.hasNext()){
+
+            String key = itr.next();
+            Object value = params.get(key);
+
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            Log.d("TAG",result.toString());
+            result.append(URLEncoder.encode(key, "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(value.toString(), "UTF-8"));
+        }
+        return result.toString();
     }
 }
