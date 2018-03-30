6dafd01588f4f24e4ca87696dabbb60972e04b66
Yash007 <sompurayash@live.com>
2018-03-30 15:00:56 -0400

    Fragment created for Events

---
+++ app/src/main/java/com/example/yash007/sportsapplication/EventFragment.java
@@ -0,0 +1,23 @@
+package com.example.yash007.sportsapplication;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class EventFragment extends android.support.v4.app.Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_event,container,false);
+    }
+
+
+}
