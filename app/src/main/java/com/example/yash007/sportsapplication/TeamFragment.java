bdfa04b662c9aa9e4831c7fc5971e6ce45984151
Yash007 <sompurayash@live.com>
2018-03-30 14:59:23 -0400

    Fragment Created for Team

---
+++ app/src/main/java/com/example/yash007/sportsapplication/TeamFragment.java
@@ -0,0 +1,21 @@
+package com.example.yash007.sportsapplication;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class TeamFragment extends android.support.v4.app.Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_team,container, false);
+    }
+
+}
