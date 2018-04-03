bb1e68b0661f5e6c2d965c03faff7fc37b31732c
Yash007 <sompurayash@live.com>
2018-04-03 16:49:48 -0400

    Add Event Activity Added in Events Fragment

--- app/src/main/java/com/example/yash007/sportsapplication/EventFragment.java
+++ app/src/main/java/com/example/yash007/sportsapplication/EventFragment.java
@@ -1,10 +1,13 @@
 package com.example.yash007.sportsapplication;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,10 +16,36 @@ import android.view.ViewGroup;
 
 public class EventFragment extends android.support.v4.app.Fragment {
 
+    private FloatingActionButton addEventButton;
+
+    Activity context;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_event,container,false);
+        View view = inflater.inflate(R.layout.fragment_event, container, false);
+
+        context = getActivity();
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        addEventButton = (FloatingActionButton) context.findViewById(R.id.addEventButton);
+
+        addEventButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, CreateEventActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
 
--- app/src/main/java/com/example/yash007/sportsapplication/TeamFragment.java
+++ app/src/main/java/com/example/yash007/sportsapplication/TeamFragment.java
@@ -1,21 +1,29 @@
 package com.example.yash007.sportsapplication;
 
+import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 
 public class TeamFragment extends android.support.v4.app.Fragment {
 
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_team,container, false);
+        View view = inflater.inflate(R.layout.fragment_team, container, false);
+        return view;
     }
 
     @Override
